f3124a59b0da417ec195d96bb5b813c74ada1b35
whohardik <hardickupadhyay008@gmail.com>
2022-07-18 12:47:04 +0530

    6th

---
+++ src/Models/urlModel.js
@@ -0,0 +1,23 @@
+
+const mongoose = require("mongoose");
+
+const urlSchema = new mongoose.Schema({
+  longUrl: {
+    type: String,
+    required: true,
+  },
+  shortUrl: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  urlCode: {
+    type: String,
+    require: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+},{timestamps: true});
+
+module.exports = mongoose.model("Url", urlSchema);
